ea177fa27890c995217880ca51680a68516f65e0
MugheesNadeem <mughees.nadeem@txlabz.com>
2021-02-10 21:54:28 +0500

    First commit

---
+++ lib/model/provider_side.dart
@@ -0,0 +1,20 @@
+class ProviderSide {
+  String title;
+  String subtitle;
+  String timeDuration;
+  String createdTime;
+  String repetition;
+  String userName;
+  double userRating;
+  int stars;
+
+  ProviderSide(
+      {this.title,
+      this.subtitle,
+      this.timeDuration,
+      this.createdTime,
+      this.repetition,
+      this.userName,
+      this.userRating,
+      this.stars});
+}
